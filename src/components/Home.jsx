04fae9118f4437b8bfc4d8d171830ee6a0835c2a
Abdul Razzak <abdulrazzak74832@gmail.com>
2023-03-27 00:40:07 +0530

    done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,6 +8,7 @@ const Home = () => {
         <main className="grow flex justify-center">
             <div className="w-full px-5 flex flex-col items-center mt-44">
                 <img src={Logo} alt="logo" className="w-[172px] md:w-[272px] " />
+                {/* input section */}
                 <SearchInput />
                 <div className="flex gap-2 text-[#3c4043] mt-8">
                     <button className="h-9 px-4 bg-[#f8f9fa] text-sm rounded-md border border-[#f8f9fa] hover:border-[#dadce0] hover:shadow-c2">Google Search</button>
